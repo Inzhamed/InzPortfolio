c032ca80b45199f5a9e7ddd5a842ed296cdfb358
INEZARENE Hamed Abdelaziz <95131724+Inzhamed@users.noreply.github.com>
2024-07-19 00:27:25 +0000

    chore: Update personal information and image in Home and About components

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,25 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a full-stack web developer and a third-year computer science student at University of Science And Technology Houari Boumediene.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Es6 JavaScript",
+  "React",
+  "Next.js",
+  "Tailwind CSS",
+  "Three.js",
+  "Node.js",
+  "Express",
+  "MongoDB",
+  "Oracle SQL",
+  "Spring Boot Java",
+  "Ruby on Rails",
+  "Python",
 ];
 
 /**
@@ -45,7 +51,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about Artificial Intelligence and its potential to revolutionize the way we interact with technology. I strive to leverage my full-stack development skills to build innovative and impactful solutions.";
 
 const About = () => {
   return (
@@ -56,6 +62,7 @@ const About = () => {
           backgroundColor: "white",
           width: "50%",
           padding: "4rem",
+          paddingTop: "2rem",
           margin: "3rem auto",
           textAlign: "center",
         }}
